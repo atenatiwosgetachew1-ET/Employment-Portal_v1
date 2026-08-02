97b9157a0a887d40660a71ee502e2adf9e2f1778
atenatiwosgetachew1-ET <atenatiwosgetachew1@gmail.com>
2026-08-03 00:07:34 +0300

    02-08-2026

--- backend/portal/portal/urls.py
+++ backend/portal/portal/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app.urls')),  # this makes /api/login/ work
+    path('api/', include('app.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
