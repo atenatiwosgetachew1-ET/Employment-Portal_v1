3d905409973ec7a8583b49b1750295338233457d
atenatiwosgetachew1-ET <atenatiwosgetachew1@gmail.com>
2026-08-02 18:52:15 +0300

    02-08-2026

--- backend/portal/portal/settings.py
+++ backend/portal/portal/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = _env_bool("DEBUG", True)
 
-ALLOWED_HOSTS = _env_list("ALLOWED_HOSTS","https://employment-portal-tdd0.onrender.com","localhost,127.0.0.1")
+ALLOWED_HOSTS = _env_list("ALLOWED_HOSTS","https://employment-portal-tdd0.onrender.com,localhost,127.0.0.1")
 
 
 # Application definition
